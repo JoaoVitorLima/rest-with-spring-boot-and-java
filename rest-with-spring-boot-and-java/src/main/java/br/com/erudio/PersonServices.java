df0dc711fc4f6f9009368773b563f1e5af0f4aec
JoaoVitorLima <joaovitorlo@outlook.com>
2025-10-18 15:28:19 -0300

    New Http Methods

--- rest-with-spring-boot-and-java/src/main/java/br/com/erudio/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/erudio/PersonServices.java
@@ -1,4 +1,62 @@
 package br.com.erudio;
 
+import br.com.erudio.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
 public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding all People!");
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    public Person findById(String id) {
+        logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("João");
+        person.setLastName("Oliveira");
+        person.setAddress("Belo Horizonte - Minas Gerais - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one Person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Some address in Brazil");
+        person.setGender("Male");
+        return person;
+    }
 }
