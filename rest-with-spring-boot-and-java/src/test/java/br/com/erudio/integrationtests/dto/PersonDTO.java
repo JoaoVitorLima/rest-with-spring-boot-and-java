25dce8d0ee1f14bec97a9fd2f4caf99d947921a8
JoaoVitorLima <joaovitorlo@outlook.com>
2026-02-16 13:38:29 -0300

    Added Many to Many relation to Person and PersonDTO repositories

--- rest-with-spring-boot-and-java/src/test/java/br/com/erudio/integrationtests/dto/PersonDTO.java
+++ rest-with-spring-boot-and-java/src/test/java/br/com/erudio/integrationtests/dto/PersonDTO.java
@@ -1,9 +1,11 @@
 package br.com.erudio.integrationtests.dto;
 
+import br.com.erudio.model.Book;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 @XmlRootElement(name = "person")  // For JAXB
@@ -18,6 +20,10 @@ public class PersonDTO implements Serializable {
     private String address;
     private String gender;
     private Boolean enabled;
+
+    private String profileUrl;
+    private String photoUrl;
+    private List<Book> books;
 
     public PersonDTO() {
     }
@@ -70,15 +76,38 @@ public class PersonDTO implements Serializable {
         this.enabled = enabled;
     }
 
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        PersonDTO personDTO = (PersonDTO) o;
-        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender()) && Objects.equals(getEnabled(), personDTO.getEnabled());
+        if (!(o instanceof PersonDTO personDTO)) return false;
+        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender()) && Objects.equals(getEnabled(), personDTO.getEnabled()) && Objects.equals(getProfileUrl(), personDTO.getProfileUrl()) && Objects.equals(getPhotoUrl(), personDTO.getPhotoUrl()) && Objects.equals(getBooks(), personDTO.getBooks());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender(), getEnabled());
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender(), getEnabled(), getProfileUrl(), getPhotoUrl(), getBooks());
     }
 }
